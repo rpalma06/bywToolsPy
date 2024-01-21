37a0f13b0276443e9757e0b85d67619b75fac464
rpalma06 <cuentas@raulpalma.info>
2024-01-21 19:28:41 +0100

    Code finished no comment

--- GetClicks.py
+++ GetClicks.py
@@ -2,15 +2,15 @@ import re
 import sys
 import datetime
 
-import numpy as np
-
 import Classification
 import Click
 import Coordinate
-
+import io
+import os
 
 from astropy.io import fits
 from astropy.wcs import WCS
+from astropy.wcs import utils
 
 FRAME_PREFIX = "\"\"frame\"\":"
 
@@ -26,51 +26,152 @@ NATURAL_HEIGHT_PREFIX = "\"\"naturalHeight\"\":"
 
 
 def main():
+
     args = sys.argv[1:]
     wcs = None
     try:
-        hdu_list = fits.open("F:\\UCD\\candidates\\astrom-atlas.fits")
-        hdu_list_info = hdu_list.info()
-        header = hdu_list[0].header
+        input_filename = "F:\\UCD\\candidates\\astrom-atlas.fits"
+        output_filename = "F:\\UCD\\candidates\\clicks_ra_dec.csv"
+        wcs_filename = "F:\\UCD\\candidates\\astrom-atlas.fits"
+        try:
+            if len(args) > 0:
+                input_filename = args[0]
+            if len(args) > 1:
+                output_filename = args[1]
+            if len(args) > 2:
+                wcs_filename = args[2]
+        except:
+            pass
+        hdu_list = fits.open(wcs_filename)
         data = hdu_list[1].data
         tile_list = []
+        origin_list = []
         for row in data:
-            naxis_array = [int(row["NAXIS"][0]), int(row["NAXIS"][1])]
-            w = WCS(naxis=len(row["NAXIS"]))
+            w = WCS(naxis=2)
+
             w.wcs.cd = row['CD']
-            #w.wcs.cdelt = row['CDELT']
             w.wcs.crpix = row['CRPIX']
             w.wcs.crval = row["CRVAL"]
             w.wcs.ctype = row["CTYPE"]
             w.wcs.latpole = row["LATPOLE"]
+            w.wcs.lonpole = row["LONGPOLE"]
             tile_list.append(w)
+            origin_list.append(row["CDELT"][0])
     finally:
         hdu_list.close()
 
-    read_candidates(tile_list)
+    process_candidates(tile_list, origin_list, input_filename, output_filename)
 
 
+def get_wcs_tiles(wcs_path: str) -> list[WCS]:
+    hdu_list = None
+    try:
+        hdu_list = fits.open(wcs_path)
+        data = hdu_list[1].data
+        tile_list = []
+        origin_list = []
+        for row in data:
+            naxis_array = [int(row["NAXIS"][0]), int(row["NAXIS"][1])]
+            naxis = len(row["NAXIS"])
+            w = WCS(naxis=2)
 
+            w.wcs.cd = row['CD']
+            # w.wcs.cdelt = row['CDELT']
+            w.wcs.crpix = row['CRPIX']
+            w.wcs.crval = row["CRVAL"]
+            w.wcs.ctype = row["CTYPE"]
+            w.wcs.latpole = row["LATPOLE"]
+            w.wcs.lonpole = row["LONGPOLE"]
+            tile_list.append(w)
+            origin_list.append(row["CDELT"][0])
+    finally:
+        if hdu_list is not None:
+            hdu_list.close()
+    return tile_list
 
-
-def read_candidates(tile_list: list):
-    path = "F:\\UCD\\candidates\\backyard-worlds-planet-9-classifications.1.5.24.csv"
-    csv = open(path, mode="r", encoding="UTF-8")
+def process_candidates(tile_list: list, origin_list: list, input_path: str, output_path: str):
+    input_path = "F:\\UCD\\candidates\\backyard-worlds-planet-9-classifications.1.5.24.csv"
+    output_path = "F:\\UCD\\candidates\\"
+    if not output_path.endswith(os.sep):
+        output_path = output_path + os.sep
+    out_prefix = "clicks_ra_dec"
+    out = None
+    csv = open(input_path, mode="r", encoding="UTF-8")
     try:
         header = csv.readline()
         line = ''
         count = 0
-        while line is not None and count < 12000:
+        file_suffix = None
+        while line is not None:
             line = csv.readline()
-            classification_data = extract_classification_data(line)
+            classification = extract_classification_data(line)
             clicks = extract_clicks(line)
             sub_tile_center = extract_sub_tile_center(line)
             tile_number = extract_tile_number(line)
-            if classification_data is not None and clicks is not None and len(clicks) > 0 and sub_tile_center is not None and tile_number is not None:
+
+            if classification is not None and clicks is not None and len(clicks) > 0 and len(clicks) <= 20 and sub_tile_center is not None and tile_number is not None:
+                common = io.StringIO()
+                common.write(str(classification.classification_id))
+                common.write(";")
+                common.write(classification.user_name)
+                common.write(";")
+                common.write(classification.user_id)
+                common.write(";")
+                common.write(classification.user_ip)
+                common.write(";")
+                common.write(classification.workflow_id)
+                common.write(";")
+                common.write(classification.workflow_name)
+                common.write(";")
+                common.write(classification.workflow_version)
+                common.write(";")
+                if classification.started_at is not None:
+                    started_at_string = datetime.datetime.strftime(classification.started_at, "%Y-%m-%d %H:%M:%S") + " UTC"
+                    new_file_suffix = "_" + started_at_string[0:7]
+                    if file_suffix != new_file_suffix:
+                        file_suffix = new_file_suffix
+                        if out is not None:
+                            out.close()
+                        out = open(output_path + out_prefix + new_file_suffix + ".csv", "w")
+                        out.write("classification_id;user_name;user_id;user_ip;workflow_id;workflow_name;workflow_version;started_at;gold_standard;expert;subject_id;tile_number;tile_center_ra;tile_center_dec;click_frame;click_tool;click_x;click_y;ra;dec\n")
+                    common.write(started_at_string)
+                    common.write(";")
+                common.write(classification.gold_standard)
+                common.write(";")
+                common.write(classification.expert)
+                common.write(";")
+                common.write(str(classification.subject_id))
+                common.write(";")
+                common.write(str(tile_number))
+                common.write(";")
+                common.write(str(sub_tile_center.ra))
+                common.write(";")
+                common.write(str(sub_tile_center.dec))
+                common.write(";")
+                no_click_line = common.getvalue()
                 for click in clicks:
-                    pix = [[click.x, click.y]]
-                    world = tile_list[tile_number].wcs_pix2world(pix, 1.0)
-                    h = 0
+                    x, y = convert_zoo_to_subtile(click)
+                    origin = origin_list[tile_number]
+                    world = utils.pixel_to_skycoord(x+origin, y+origin , tile_list[tile_number], origin, "wcs")
+                    ra = world.ra.value
+                    dec = world.dec.value
+                    click_sb = io.StringIO()
+                    click_sb.write(no_click_line)
+                    click_sb.write(str(click.frame))
+                    click_sb.write(";")
+                    click_sb.write(str(click.tool))
+                    click_sb.write(";")
+                    click_sb.write(str(click.x))
+                    click_sb.write(";")
+                    click_sb.write(str(click.y))
+                    click_sb.write(";")
+                    click_sb.write(str(ra))
+                    click_sb.write(";")
+                    click_sb.write(str(dec))
+                    click_sb.write("\n")
+                    out_line = click_sb.getvalue()
+                    out.write(out_line)
+
             count += 1
             if count % 1000 == 0:
                 print(count)
@@ -83,9 +184,11 @@ def read_candidates(tile_list: list):
         print(f"Unexpected {err=}, {type(err)=}")
         raise
     except:
-        h = 0
+        print("Could not convert data to an integer.")
+        raise
     finally:
         csv.close()
+        out.close()
 
 def extract_clicks(line: str) -> list[Click.Click]:
     line_aux = line
@@ -241,6 +344,12 @@ def extract_tile_number(line: str) -> int | None:
                 pass
     return None
 
+
+def convert_zoo_to_subtile(click: Click) -> (float, float):
+    x = (click.x - 20.0) * 0.5
+    y = 256.0 - 0.5 * click.y
+
+    return x, y
 
 if __name__ == "__main__":
     main()
